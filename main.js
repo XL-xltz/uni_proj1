c9c237380c9eebcee5fc854289902badd46f2633
XL-xltz <804349447@qq.com>
2023-02-03 16:17:01 +0800

    万层home页面开发

--- main.js
+++ main.js
@@ -1,43 +1,51 @@
-import App from './App'
+import App from "./App";
 // 网络请求
-import { $http } from '@escook/request-miniprogram'
-uni.$http = $http
+import { $http } from "@escook/request-miniprogram";
+uni.$http = $http;
 // 配置请求根路径
-$http.baseUrl = 'https://www.uinav.com'
+$http.baseUrl = "https://www.uinav.com";
 // 请求开始之前做一些事情
 $http.beforeRequest = function (options) {
   uni.showLoading({
-    title: '数据加载中...',
-  })
-}
+    title: "数据加载中...",
+  });
+};
 // 全局函数
-uni.$showMsg = function(title = '请求失败',duration = 1500,icon="none") {
-    uni.showToast({
-      title,
-      duration,
-      icon,
-    })
-}
+uni.$showMsg = function (title = "请求失败", duration = 1500, icon = "none") {
+  uni.showToast({
+    title,
+    duration,
+    icon,
+  });
+};
+// ================= 请求封装 =================
+
+// get 请求
+uni.$gets = async function (api) {
+  const { data: res } = await uni.$http.get("/api/public/v1" + api);
+  if (res.meta.status !== 200) return uni.$showMsg();
+  return res;
+};
 // 请求完成之后做一些事情
 $http.afterRequest = function () {
-  uni.hideLoading()
-}
+  uni.hideLoading();
+};
 // #ifndef VUE3
-import Vue from 'vue'
-Vue.config.productionTip = false
-App.mpType = 'app'
+import Vue from "vue";
+Vue.config.productionTip = false;
+App.mpType = "app";
 const app = new Vue({
-    ...App
-})
-app.$mount()
+  ...App,
+});
+app.$mount();
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import { createSSRApp } from "vue";
 export function createApp() {
-  const app = createSSRApp(App)
+  const app = createSSRApp(App);
   return {
-    app
-  }
+    app,
+  };
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -18,16 +18,53 @@
         <image class="nav-img" :src="item.image_src"></image>
       </view>
     </view>
+
+    <!-- 楼层区域 -->
+    <view class="floor-list">
+      <!-- 楼层 item 项 -->
+      <view class="floor-item" v-for="(item, i) in floorList" :key="i">
+        <!-- 楼层标题 -->
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+
+        <!-- 楼层图片区域 -->
+        <view class="floor-img-box">
+          <!-- 左侧大图片的盒子 -->
+          <navigator class="left-img-box" :url="item.product_list[0].url">
+            <image
+              :src="item.product_list[0].image_src"
+              :style="{ width: item.product_list[0].image_width + 'rpx' }"
+              mode="widthFix"
+            ></image>
+          </navigator>
+          <!-- 右侧 4 个小图片的盒子 -->
+          <view class="right-img-box">
+            <navigator
+              class="right-img-item"
+              v-for="(item2, i2) in item.product_list"
+              :key="i2"
+              v-if="i2 !== 0"
+              :url="item2.url"
+            >
+              <image :src="item2.image_src" mode="widthFix" :style="{ width: item2.image_width + 'rpx' }"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 <script>
+// import logShim from 'npm/lib/utils/log-shim'
 export default {
   data() {
     return {
+      // '/api/public/v1/home/floordata'
       // 轮播图
       swiperList: [],
       // 分类
-      navList: []
+      navList: [],
+      // 楼层的数据列表
+      floorList: []
     }
   },
   onLoad() {
@@ -35,21 +72,18 @@ export default {
     this.getSwiperList()
     // 获取分类
     this.getNavList()
+    // 楼层图片
+    this.getFloorList()
   },
   methods: {
     // 获取轮播图
     async getSwiperList() {
-      const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
-      // 3.2 请求失败
-      if (res.meta.status !== 200) return uni.$showMsg()
-      // 3.3 请求成功，为 data 中的数据赋值
+      const res = await uni.$gets('/home/swiperdata')
       this.swiperList = res.message
     },
     // 获取分类
     async getNavList() {
-      const { data: res } = await uni.$http.get('/api/public/v1/home/catitems')
-      console.log(res)
-      if (res.meta.status !== 200) return uni.$showMsg()
+      const res = await uni.$gets('/home/catitems')
       this.navList = res.message
     },
     // 点击分类图片
@@ -59,6 +93,20 @@ export default {
           url: '/pages/cate/cate'
         })
       }
+    },
+    // 楼层图片
+    async getFloorList() {
+      // product_list navigator_url
+      const res = await uni.$gets('/home/floordata')
+      console.log(res, 'loucengtupian')
+      // 循环出 url
+      res.message.forEach(floor => {
+        floor.product_list.forEach(prod => {
+          prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+          console.log(prod.url)
+        })
+      })
+      this.floorList = res.message
     }
   }
 }
@@ -84,5 +132,20 @@ swiper {
     width: 128rpx;
     height: 140rpx;
   }
+}
+.floor-title {
+  height: 60rpx;
+  width: 100%;
+  display: flex;
+}
+.right-img-box {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.floor-img-box {
+  display: flex;
+  padding-left: 10rpx;
 }
 </style>
